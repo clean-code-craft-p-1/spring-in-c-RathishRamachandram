35e303d8fa14797c99f5641ec6a91b549a96159b
RathishRamachandram <rathish.r@philips.com>
2023-07-25 21:37:24 +0530

    Initial

--- stats.cpp
+++ stats.cpp
@@ -2,7 +2,42 @@
 
 struct Stats compute_statistics(const float* numberset, int setlength) {
     struct Stats s;
+    int counter = 0;
+    float sum = 0;
+    float min = numberset[counter];
+    float max = numberset[counter];
+
     s.average = 0;
     s.min = 0;
     s.max = 0;
+
+    if (numberset != NULL && setlength != 0)
+    {
+        for (counter = 0; counter < setlength; cout++)
+        {
+            sum += numberset[counter];
+
+            if (numberset[counter] < min)
+            {
+                min = numberset[counter];
+            }
+
+            if (numberset[counter] > max)
+            {
+                max = numberset[counter];
+            }
+        }
+
+        s.average = (float)sum / setlength;
+        s.min = min;
+        s.max = max;
+    }
+    esle
+    {
+        s.average = NAN;
+        s.min = NAN;
+        s.max = NAN;
+    }
+
+    return s;
 }
--- stats.h
+++ stats.h
@@ -1,7 +1,12 @@
+#ifndef _STATS_H
+#define _STATS_H
 
+#include <math.h>
 // define the Stats structure here. See the tests to infer its properties
 
 struct Stats compute_statistics(const float* numberset, int setlength);
 
 typedef void (*alerter_funcptr)();
 void check_and_alert(float maxThreshold, alerter_funcptr alerters[], struct Stats computedStats);
+
+#endif
