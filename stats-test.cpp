f61d01b1ebf007aba951cf7510d73ccc3fe2ae3e
RathishRamachandram <rathish.r@philips.com>
2023-07-26 20:52:42 +0530

    Stage11

--- stats-test.cpp
+++ stats-test.cpp
@@ -24,9 +24,9 @@ TEST(Statistics, AverageNaNForEmpty) {
     
     //Design the EXPECT statement here.
     //Use https://stackoverflow.com/questions/1923837/how-to-use-nan-and-inf-in-c
-    EXPECT_LT(computedStats.average, NAN);
-    EXPECT_LT(computedStats.max, NAN);
-    EXPECT_LT(computedStats.min, NAN);
+    EXPECT_EQ(computedStats.average, NAN);
+    EXPECT_EQ(computedStats.max, NAN);
+    EXPECT_EQ(computedStats.min, NAN);
 }
 
 int emailAlertCallCount = 0;
